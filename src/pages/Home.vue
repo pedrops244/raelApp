ec15789fac4863528eadea8d32377125bf1e0c1a
Pedro Souza <pedrops244@hotmail.com>
2024-02-04 14:33:46 -0300

    create a initial homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,50 @@
 <script setup></script>
 
 <template>
-  <div class="mt-50">
-    <h1 class="d-flex justify-center align-center">Profile Users</h1>
-  </div>
+  <section class="section">
+    <div class="section-content">
+      <h1>Coach Rael | Consultoria Fitness</h1>
+      <p>
+        Independentemente do seu nível de condicionamento físico ou experiência
+        anterior, estamos aqui para apoiá-lo em cada passo do caminho. Junte-se
+        a nós e embarque nesta jornada de transformação pessoal rumo a uma vida
+        mais saudável e feliz!
+      </p>
+    </div>
+  </section>
 </template>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+.section {
+  height: 100vh;
+  background-image: url('../images/background-home.jpg');
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+}
+
+.section-content {
+  max-width: 800px;
+  padding: 20px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+}
+
+h1 {
+  font-size: 48px;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 18px;
+  line-height: 1.6;
+}
+</style>
