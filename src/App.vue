c2a978e7ab5435e4f51a7063aba722d0d8212226
Pedro Souza <pedrops244@hotmail.com>
2024-02-04 14:47:16 -0300

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ThisMenu from '@/components/Menu.vue';
+import ThisFooter from './components/Footer.vue';
 </script>
 <template>
   <v-app>
     <v-main>
       <ThisMenu />
       <router-view></router-view>
+      <ThisFooter />
     </v-main>
   </v-app>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-footer class="bg-red-darken-1 text-center d-flex flex-column">
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="text"
+      ></v-btn>
+    </div>
+
+    <div class="pt-0">
+      Entre em contato conosco hoje mesmo para agendar uma consulta inicial e
+      dar o primeiro passo em direção a uma versão melhor de si mesmo.
+    </div>
+
+    <v-divider></v-divider>
+
+    <div>{{ new Date().getFullYear() }} — <strong>CoachRael</strong></div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+  }),
+};
+</script>
