22707680cb8a53efaae558eb64a34279d7793f4f
Pedro Souza <pedrops244@hotmail.com>
2024-02-07 18:42:27 -0300

    editing homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,33 @@
 
 <template>
   <v-container class="d-flex text-center justify-center">
-    <v-card width="860" height="500" elevation="5">
-      <p class="text-h1 pa-5">
-        Coach Rael
-        <v-divider class="border-opacity-50"></v-divider>
-      </p>
-      <p class="text-h3 pb-3">Consultoria Fitness</p>
-      <p class="text-subtitle-2">
-        Independentemente do seu nível de condicionamento físico ou experiência anterior, estamos
-        aqui para apoiá-lo em cada passo do caminho. Junte-se a nós e embarque nesta jornada de
-        transformação pessoal rumo a uma vida mais saudável e feliz!
-      </p>
-    </v-card>
+    <v-row>
+      <v-col>
+        <v-sheet>
+          <v-img cover src="../images/home-image-rael.png"></v-img>
+        </v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-3 ma-3">
+          <p class="text-h1 pa-3">
+            Coach Rael
+            <v-divider class="border-opacity-50"></v-divider>
+            <v-spacer></v-spacer>
+          </p>
+          <p class="text-subtitle-2">
+            Avaliar, organizar, prescrever e orientar a prática de atividades físicas estruturadas
+            com base em princípios do treinamento desportivo buscando os objetivos e necessidades
+            específicas do indivíduo é uma das marcas do trabalho de Israel Hidequel.
+          </p>
+          <p class="text-subtitle-2">
+            Ao longo do processo de treinamento, um personal trainer oferece apoio contínuo e
+            incentivo para manter seus clientes motivados e no caminho certo. Eles demonstram
+            técnicas de exercícios adequadas, fornecem feedback sobre a forma e ajustam a
+            intensidade do treino conforme necessário para garantir segurança e eficácia.
+          </p>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <style></style>
